503c6a5c8f3e194b34613426c46ac4fe352121d1
Harriet Craven <harrietcraven@Harriets-MacBook-Pro.local>
2015-09-23 12:35:41 +0100

    added home page

--- app/app.rb
+++ app/app.rb
@@ -3,6 +3,10 @@ require_relative 'data_mapper_setup'
 
 class BookmarkManager < Sinatra::Base
 
+  get '/' do
+    erb :homepage
+  end
+
   get '/links' do
     @links = Link.all
     erb :'links/index'
