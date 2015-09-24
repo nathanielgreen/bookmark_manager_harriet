7bbf1181b25a07b168d76ec4f8efeb683103471a
adellanno <anthony@dellanno.co.uk>
2015-09-24 16:00:33 +0100

    added password confirmation

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ source "https://rubygems.org"
 # gem "rails"
 ruby '2.2.1'
 gem 'sinatra'
+gem 'sinatra-flash'
 gem 'bcrypt'
 gem 'data_mapper'
 gem 'dm-postgres-adapter'
--- app/app.rb
+++ app/app.rb
@@ -1,9 +1,12 @@
 require 'sinatra/base'
 require_relative 'data_mapper_setup'
+require 'sinatra/flash'
+
 
 class BookmarkManager < Sinatra::Base
 
   enable :sessions
+  register Sinatra::Flash
   set :session_secret, 'super secret'
 
   helpers do
@@ -44,15 +47,21 @@ class BookmarkManager < Sinatra::Base
   end
 
   get '/users/new' do
+    @user = User.new
     erb :'users/new'
   end
 
   post '/users' do
-    user = User.create(email: params[:email],
+    @user = User.create(email: params[:email],
       password: params[:password],
       password_confirmation: params[:password_confirmation])
-    session[:user_id] = user.id
-    redirect to('/links')
+    if @user.save
+      session[:user_id] = @user.id
+      redirect to('/links')
+    else
+      flash.now[:notice] = "Password and confirmation password do not match"
+      erb :'users/new'
+    end
   end
 
 run! if app_file == BookmarkManager
